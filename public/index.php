d373bef93a81e8db8566c214b68f391ce92f3549
Luis Andrés Arce C <luilliarcec@gmail.com>
2019-05-26 16:14:47 -0500

    Commit for Heroku

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ session_start();
 $dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
 $dotenv->load();
 
-define('BASE_URL', '/Platzi-IntroduccionPHP-2018/');
+define('BASE_URL', '/');
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Aura\Router\RouterContainer;
@@ -20,8 +20,9 @@ use Aura\Router\RouterContainer;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver' => 'mysql',
+    'driver' => getenv('DB_DRIVER'),
     'host' => getenv('DB_HOST'),
+    'port' => getenv('DB_PORT'),
     'database' => getenv('DB_NAME'),
     'username' => getenv('DB_USER'),
     'password' => getenv('DB_PASS'),
